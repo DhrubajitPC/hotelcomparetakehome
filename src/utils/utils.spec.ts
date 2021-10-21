e9b45d9e64c7d1376fa931c52e957c6e4953fddd
Dhrubajit <dhrubajit1992@gmail.com>
2021-10-21 17:35:22 +0800

    fix util test for formatPrice

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -2,8 +2,8 @@ import { formatPrice } from ".";
 describe("#Utils", () => {
   it("Should format price for currencies like USD, SGD, CNY by rounding to their nearest dollar", () => {
     const sgd = formatPrice("SGD", 1000012.1251);
-    const usd = formatPrice("SGD", 1000012.1251);
-    const cny = formatPrice("SGD", 1000012.1251);
+    const usd = formatPrice("USD", 1000012.1251);
+    const cny = formatPrice("CNY", 1000012.1251);
 
     expect(sgd).toBe(1000012);
     expect(usd).toBe(1000012);
